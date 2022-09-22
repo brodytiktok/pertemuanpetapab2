469ff414f565422e7085c92cacdf9a0fe9c9323f
YandD9 <105333102+brodytiktok@users.noreply.github.com>
2022-09-22 15:17:48 +0700

    a

---
+++ Peta/app/src/main/java/com/if3a/peta/MainActivity.java
@@ -0,0 +1,43 @@
+package com.if3a.peta;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.if3a.peta.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+    private ActivityMainBinding binding;
+    private GoogleMap mMap;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+
+
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        mMap = googleMap;
+        //mMap.setMyLocationEnabled(true);
+        LatLng latLngUser = new LatLng(-2.9483130928394026, 104.77538045835556);
+        mMap.addMarker(new MarkerOptions().position(latLngUser).title("Lokasi Saya"));
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLngUser, 19));
+    }
+}
